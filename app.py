71a42c06495f782fb1b33a47b7985b9665a489c1
Tony Vu <tonyvu2008@gmail.com>
2016-10-31 23:49:13 +0800

    Update template

--- app.py
+++ app.py
@@ -39,6 +39,6 @@ def view_category():
 app.wsgi_app = ProxyFix(app.wsgi_app)            
 
 if __name__ == "__main__":
-    port = int(os.getenv('PORT', 5000))
+    port = int(os.getenv('PORT', 8000))
     app.run(host='0.0.0.0', port=port, threaded=True)
     
