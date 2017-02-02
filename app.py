e5feb346f011a137345fc974f9eed2e83678cb3e
Tony Vu <tonyvu2008@gmail.com>
2017-02-02 23:44:38 +0800

    Add set_token page

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import os
 import redis
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 from read_news_feed import (
     read_config_all_subscribe_news_feed_in_parallel, 
     read_db_all_subscribe_news_feed_in_parallel
 )
 from common import const   
-from db_manager import add_to_list, get_list
+from db_manager import add_to_list, get_list, set_value, get_value
 from werkzeug.contrib.fixers import ProxyFix
        
 
@@ -35,6 +35,18 @@ def add_category(category):
 def view_category():   
     categories = get_list(const.CATEGORY)
     return 'Categories: {}'.format(",".join(categories))
+    
+    
+@app.route("/set_token")
+def set_token():
+    return render_template('token_form.html')
+    
+
+@app.route("/update_token/", methods=['POST'])    
+def update_token():
+    set_value('token', request.form['token']);
+    return redirect(url_for('main'))
+    
     
 app.wsgi_app = ProxyFix(app.wsgi_app)            
 
